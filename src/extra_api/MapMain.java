a4b068c5eecde549ab201e86f201b074c4028cc4
JuyoungKwak0618 <rmmr123@naver.com>
2024-01-31 10:10:35 +0900

    ..

---
+++ src/extra_api/MapMain.java
@@ -0,0 +1,21 @@
+package extra_api;
+
+import java.util.Map;
+
+public class MapMain {
+
+	public static void main(String[] args) throws Exception {
+		RoadAddrApi roadApi = new RoadAddrApi();
+		kakaoLocalApi kakaoApi = new kakaoLocalApi();
+		
+		String[] places = {"경기도청", "팔달구청", "장안구청", "영통구청", "권선구청"};
+		for(String place:places) {
+			String roadAddr = roadApi.getRoadAddr(place);
+			Map<String, Double> geoCode = kakaoApi.getGeocode(roadAddr);
+			System.out.printf("%s: %s, %.6f, %6f%n", place,roadAddr, geoCode.get("lat"),geoCode.get("lon"));
+			
+		}
+
+	}
+
+}
