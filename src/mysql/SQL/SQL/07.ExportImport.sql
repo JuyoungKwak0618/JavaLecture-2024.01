32b1e69ebb72397062e6dad26086a00ec2b73f5a
JuyoungKwak0618 <rmmr123@naver.com>
2024-02-05 10:04:59 +0900

    ..

--- src/mysql/SQL/SQL/07.ExportImport.sql
+++ src/mysql/SQL/SQL/07.ExportImport.sql
@@ -17,9 +17,9 @@ SHOW VARIABLES LIKE 'secure_file_priv';
 */ 
 SELECT * FROM song INTO OUTFILE
 	'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/song.csv'
-	FIELDS TERMINATED BY ','
-	ENCLOSED BY '"'
-	LINES TERMINATED BY '\n';
+	FIELDS TERMINATED BY ','                    # csv 파일안 컬럼 구분자 지정
+	ENCLOSED BY '"'                             #  각 컬럼/필드는 ""로 감싸져 있음
+	LINES TERMINATED BY '\n';                   # 각 라인 구분은 \n
 
 /*
 * 3. Import ()
@@ -29,10 +29,12 @@ truncate song;
 
 # 데이터 로드
 LOAD DATA INFILE
-	'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/song.csv'
-	INTO TABLE song
+	'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/song.csv'    # 읽어 들일 파일 지정
+	INTO TABLE song                                             # load할 테이블 선택
 	FIELDS TERMINATED BY ','
 	ENCLOSED BY '"'
 	LINES TERMINATED BY '\n';
+
+# 데이터 확인
 	
 
